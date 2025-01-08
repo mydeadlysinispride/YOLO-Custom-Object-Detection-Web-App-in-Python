1f57bfbb216bd1c5cb5806e15b680ef24561a1e4
unknown <namlc321@gmail.com>
2025-01-08 16:07:44 +0700

    update setup for package installation

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.ppeDetection import logger
+from src.ppeDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Logging from main.py")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
